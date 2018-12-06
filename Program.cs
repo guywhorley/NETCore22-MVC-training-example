10ba561715cf8fc2091954a98b56c8d83fb92f6c
Guy Whorley <guy.whorley@cherwell.com>
2018-12-06 15:13:00 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OdeToFood
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateWebHostBuilder(args).Build().Run();
+		}
+
+		// Build Kestrel web server, set up IIS Integration, Default logging, IConfiguration service
+		// is made available (appsettings.json, User secrets file, env vars, cli-args)
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+	}
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OdeToFood
+{
+	public class Startup
+	{
+		// Built-in DI: This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSingleton<IGreeter, Greeter>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		// Optionally, you can pass IConfiguration into Configure(). But you also have to manually add
+		// appsetting.json file to the root of the project.
+		public void Configure(IApplicationBuilder app,
+							  IHostingEnvironment env,
+							  //IConfiguration configuration)
+							  IGreeter greeter)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			
+			app.Run(async (context) =>
+			{
+				// order of settings precedence: appsettings.json, env.vars, cli-args
+				// Also... you can also pass in from cli: dotnet run Greeting="<some-new-text>" [enter]
+				//var greeting = configuration["Greeting"];
+				var greeting = greeter.GetMessageOfTheDay(); //configuration["Greeting"];
+				await context.Response.WriteAsync(greeting);
+			});
+		}
+	}
+}
