f1b9d33bff546435d55885ddb3c3acfecc00c24c
Guy Whorley <guy.whorley@cherwell.com>
2018-12-06 18:29:47 -0800

    Added mvc service and HomeController

--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,7 @@ namespace OdeToFood
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddSingleton<IGreeter, Greeter>();
+			services.AddMvc();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,8 +62,10 @@ namespace OdeToFood
 			//	Path="/wp"
 			//});
 
-			app.UseFileServer();
-			
+			app.UseStaticFiles();
+
+			app.UseMvcWithDefaultRoute();
+
 			app.Run(async (context) =>
 			{
 				
